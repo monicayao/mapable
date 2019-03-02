2ef8e80e2e2efa45af49cff38fd4cbcbac3590cd
Jacob Boerma <jacobswb2@gmail.com>
2019-03-02 14:31:28 -0800

    Implemented Uniform Cost Search to find route given distance data. For now, return one ordered list of locations as a JSON object

--- server/server.py
+++ server/server.py
@@ -1,16 +1,97 @@
 import http.server
 import socketserver
 import json
+import queue
+import pprint
+import copy
 
 PORT = 8000
+pp = pprint.PrettyPrinter()
 
 # The path algorithm
 def pathAlg(jsonObj):
     startNode = jsonObj["startLocation"]
+    endNode = jsonObj["endLocation"]
     startHourStr = jsonObj["startTime"]
-    waitTime = {}
-    for key in 
-    # create complete graph
+    totalTime = jsonObj["totalTime"]
+    waitTimes = jsonObj["listOfLocations"]
+
+    distanceMatrix = {}
+    for source, dest, distance in jsonObj["distanceInfo"]:
+        if source not in distanceMatrix:
+            distanceMatrix[source] = {}
+        distanceMatrix[source][dest] = distance
+
+        source, dest = dest, source
+        if source not in distanceMatrix:
+            distanceMatrix[source] = {}
+        distanceMatrix[source][dest] = distance
+
+    # pp.pprint(distanceMatrix)
+    nodes = list(waitTimes.keys())
+    paths = uniformCostSearch(startNode, endNode, waitTimes, totalTime, distanceMatrix, 10)
+
+    pp.pprint(paths)
+
+    paths = [path for cost, path in list(reversed(paths))]
+
+    return paths[0]
+
+
+# returns lists of lists of paths from root
+def uniformCostSearch(startLoc, endLoc, waitTimes, maxTime, distanceMatrix, numPaths):
+    visited = set()
+
+    priorityQueue = queue.PriorityQueue()
+    goodPaths = queue.PriorityQueue(numPaths)
+
+    # objects of form
+    # (cost, ("nameOfLocation", [visited1, visited2]))
+    # PriorityQueue takes in objects of form (cost, object), min cost first
+
+    priorityQueue.put_nowait((0, (startLoc, [startLoc])))
+
+    while not priorityQueue.empty():
+        timeTaken, (location, path) = priorityQueue.get_nowait()
+        # print(path)
+
+        savedPath = tuple(sorted(path))
+        if savedPath in visited:
+            continue
+
+        visited.add(savedPath)
+
+        if (timeTaken > maxTime):
+            return unwrapPriorityQueue(goodPaths)
+
+        # guaranteed to visit least cost first
+        if (location == endLoc):
+            # get rid worst path
+            if (goodPaths.full()):
+                goodPaths.get_nowait()
+            goodPaths.put_nowait((timeTaken, path))
+            continue
+
+        # we want to look at each child, so long as it has not been visited
+        for nextLocation, timeToLoc in distanceMatrix[location].items():
+            if nextLocation in path:
+                continue
+
+            # update path with the location we're at now
+            newPath = copy.deepcopy(path)
+            newPath.append(nextLocation)
+
+            totalTime = timeTaken + waitTimes[location] + timeToLoc
+
+            priorityQueue.put_nowait((totalTime, (nextLocation, newPath)))
+
+    return unwrapPriorityQueue(goodPaths)
+
+def unwrapPriorityQueue(goodPaths):
+    paths = []
+    while not goodPaths.empty():
+        paths.append(goodPaths.get_nowait())
+    return paths
 
 class HTTPHandler(http.server.BaseHTTPRequestHandler):
     # every HTTP response needs a header
@@ -31,8 +112,6 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
     def do_HEAD(self):
         self._set_headers()
 
-    # valid json to send:
-    # "{\"hi\": \"time for bed\"}"
     def do_POST(self):
         print("received POST")
 
@@ -40,7 +119,7 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
         post_body = self.rfile.read(content_len).decode('utf-8')
         input_obj = json.loads(post_body)
         print(input_obj)
-        
+
         result = pathAlg(input_obj)
 
         self._set_headers()
